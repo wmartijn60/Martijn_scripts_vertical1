9072e1ab323cc49e50a803029eab7192cc7c0238
DESKTOP-0BTH82I\Gebruiker <mjakobs11@hotmail.nl>
2018-12-20 14:07:51 +0100

    mijn scripts

---
+++ ManaSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using Boo.Lang;
+using UnityEngine;
+
+/// <summary>
+/// checks mana and determines if card is playable
+/// </summary>
+public class ManaSystem : MonoBehaviour
+{
+    private int mana = 1;
+    public bool playable;
+    public int cardMana;
+    public int Mana
+    {
+        get
+        {
+            return mana;
+        }
+        
+        set
+        {
+
+            if (value >= cardMana)
+            {
+                playable = true;
+               
+            }
+            else
+            {
+                playable = false;
+            }
+        }
+    }
+}
---
+++ TurnHistoryIcon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class TurnHistoryIcon : MonoBehaviour
+{
+    /// <summary>
+    /// Card movement with itween.
+    /// </summary>
+	public void CardHistory ()
+	{ 
+	    iTween.MoveTo(this.gameObject, iTween.Hash("position", new Vector3(-6.46f, 0.42f, 1.582f), "time", 1f, "easeType", iTween.EaseType.easeInOutSine));
+	}
+}
